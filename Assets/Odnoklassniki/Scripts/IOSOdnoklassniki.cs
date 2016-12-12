728ff6dd8da9c62258b7a574c3685ec5db144a1d
Sergey Trembach <trembo@gmail.com>
2016-12-12 13:38:21 +0200

    1.0.24 Removed expiration times for access tokens. Usage of LONG_ACCESS_TOKEN is advised. (#55)

--- Assets/Odnoklassniki/Scripts/IOSOdnoklassniki.cs
+++ Assets/Odnoklassniki/Scripts/IOSOdnoklassniki.cs
@@ -74,8 +74,6 @@ namespace Odnoklassniki
 			}
 			AccessToken = args[0];
 			RefreshToken = args[1];
-			AccessTokenExpiresAt = DefaultAccessTokenExpires();
-			RefreshTokenExpiresAt = DefaultRefreshTokenExpires();
 			authRequested = OKAuthType.None;
 			AuthType = OKAuthType.SSO;
 
